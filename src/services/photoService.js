2c3f0a00a81410163bc6baa22b69f36f45b7401f
Bruno Barretta <bruno.barretta@fatec.sp.gov.br>
2023-08-01 21:08:53 -0300

    Initial commit

---
+++ src/services/photoService.js
@@ -0,0 +1,97 @@
+import { api, requestConfig } from "../utils/config";
+
+// Publish an user photo
+const publishPhoto = async (data, token) => {
+
+    const config = requestConfig("POST", data, token, true)
+
+    try {
+        
+        const res = await fetch(api + "/photos", config)
+        .then((res) => res.json())
+        .catch((err) => err)
+
+        return res
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// Get users photos
+const getUserPhotos = async (id, token) => {
+
+    const config = requestConfig("GET", null, token)
+
+    try {
+        
+        const res = await fetch(api + "/photos/user/" + id, config)
+        .then((res) => res.json())
+        .catch((err) => err)
+
+        return res
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// Get users photos
+const deletePhoto = async (id, token) => {
+
+    const config = requestConfig("DELETE", null, token)
+
+    try {
+        
+        const res = await fetch(api + "/photos/" + id, config)
+        .then((res) => res.json())
+        .catch((err) => err)
+
+        return res
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// Update a photo
+const updatePhoto = async (data, id, token) => {
+
+    const config = requestConfig("PUT", data, token)
+
+    try {
+        
+        const res = await fetch(api + "/photos/" + id, config)
+        .then((res) => res.json())
+        .catch((err) => err)
+
+        return res
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// Get a photo by id
+const getPhoto = async (id) => {
+
+    const config = requestConfig("GET")
+
+    try {
+        
+        const res = await fetch(api + "/photos/" + id, config)
+        .then((res) => res.json())
+        .catch((err) => err)
+
+        return res
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+const photoService = {
+    publishPhoto,
+    getUserPhotos,
+    deletePhoto,
+    updatePhoto,
+    getPhoto
+}
+
+export default photoService
