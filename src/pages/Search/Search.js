f0a7e481a58332c102165de77c4eee580eec546a
Bruno Barretta <bruno.barretta@fatec.sp.gov.br>
2023-08-07 21:29:16 -0300

    search now is working

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -12,11 +12,56 @@ import LikeContainer from "../../components/LikeContainer"
 import { Link } from "react-router-dom"
 
 //Redux
-
+import { searchPhotos, like } from '../../slices/photoSlice'
 
 const Search = () => {
+  const query = useQuery()
+  const search = query.get("q")
+
+  const dispatch = useDispatch()
+
+  const resetMessage = useResetComponentMessage(dispatch)
+
+  const {user} = useSelector((state) => state.auth)
+  const {photos, loading} = useSelector((state) => state.photo)
+
+  //Load photos
+  useEffect(() => {
+
+    dispatch(searchPhotos(search))
+
+  }, [dispatch, search])
+
+  //Like a photo
+  const handleLike = (photo) => {
+
+    dispatch(like(photo._id))
+
+    resetMessage()
+  }
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
+
+  console.log(photos)
+
   return (
-    <div>Search</div>
+    <div id='search'>
+      <h2>Você está buscando por: {search}</h2>
+      {photos && photos.map((photo) => (
+        <div key={photo._id}>
+          <PhotoItem photo={photo}/>
+          <LikeContainer photo={photo} user={user} handleLike={handleLike}/>
+          <Link className="btn" to={`/photos/${photo._id}`}>Ver mais</Link>
+        </div>
+      ))}
+      {photos && photos.length === 0 && (
+        <h2 className="no-photos">
+          Não foram encontrados resultados para sua busca.
+        </h2>
+      )}
+    </div>
   )
 }
 
